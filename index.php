665aff5af475a740c16bc22f5d8b8b1728150e78
unknown <fazil.abdulkadher@RSPAG1WN310LT.ruby-software.com>
2015-07-13 15:39:59 +0530

    adding index.php for herokuapp

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Fazicodes Template</title>
+    <!-- Reset css -->
+    <link href="css/normalize.css"rel="stylesheet">
+	
+    <link href="css/global.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <div class="wrapper">
+  <div class="container">
+  <div class="text-center">
+<h1 class="uppercase bold">Hello I’m Fazil </h1>
+<h4>Design & Build Beautiful Crafted Web Experiences</h4>
+<div class="arrow bounce">
+
+</div>
+</div>
+</div>
+</div>
+    <!-- jQuery (JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+  </body>
+</html>
