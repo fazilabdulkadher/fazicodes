59edfc721226a343187321e5212c0eff94887e53
unknown <fazil.abdulkadher@RSPAG1WN310LT.ruby-software.com>
2015-07-23 16:03:11 +0530

    adding about me section

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Fazicodes Template</title>
+    <title>Fazi Codes</title>
     <!-- Reset css -->
     <link href="css/normalize.css"rel="stylesheet">
-	
     <link href="css/global.css" rel="stylesheet">
-
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" media="(max-width: 767px)" href="css/max-767px.css">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -19,13 +20,150 @@
     <![endif]-->
   </head>
   <body>
-  <div class="wrapper">
-  <div class="container">
-  <div class="text-center">
-<h1 class="uppercase bold">Hello I’m Fazil </h1>
-<h4>Design & Build Beautiful Crafted Web Experiences</h4>
-<div class="arrow bounce">
+  <ul class="navigation hidden-desk">
+	<li class="nav-item"><a href="#">Home</a></li>
+	<li class="nav-item"><a href="#">Portfolio</a></li>
+	<li class="nav-item"><a href="#">About</a></li>
+	<li class="nav-item"><a href="#">Blog</a></li>
+	<li class="nav-item"><a href="#">Contact</a></li>
+</ul>
+<input type="checkbox" id="nav-trigger" class="nav-trigger hidden-desk" />
+<label for="nav-trigger" class="hidden-desk"></label>
 
+<div class="site-wrap">
+  <div class="my-self">
+  <div class="container">
+  <header role="banner">
+  <h1>&nbsp;</h1>
+  <nav role="navigation">
+  	<ul>
+    <li><a href="#mywork">Work</a></li>
+	<li><a href="#">About Me</a></li>
+    <li><a href="#">Writing</a></li>
+	<li><a href="#">Contact</a></li>    
+    </ul>
+  </nav>
+  </header>
+  <div class="text-center me">
+<h1 class="uppercase bold">Hello I’m BLABL </h1>
+<h4>Design & Build Beautiful Crafted Web Experiences</h4>
+<a href="#"  class="button uppercase">Hire me </a>
+<div class="arrow bounce" action="#mywork">
+</div>
+</div>
+</div>
+</div>
+<div class="my-work text-center" id="mywork">
+<div class="container" >
+<h5 class="work-h5">SOME OF MY LATEST WORK</h5>
+<div class="col col12 prt-ston">
+	<div class="col4 portfolio-cont-item">
+    	<div class="port-body">
+        	<img src="images/portfolio8.jpg">
+          </div>
+        <div class="plus-see base-bg bold">
+          +</div>
+        <div class="pot-title base-bg">
+          This is my first
+          </div>
+            <a href="#" class="overlay"></a>
+         
+    </div>
+    <div class="col4 portfolio-cont-item">
+    	<div class="port-body">
+        	<img src="images/portfolio8.jpg">
+          </div>
+        <div class="plus-see base-bg bold">
+          +</div>
+        <div class="pot-title base-bg">
+          This is my first
+          </div>
+            <a href="#" class="overlay"></a>
+         
+    </div><div class="col4 portfolio-cont-item">
+    	<div class="port-body">
+        	<img src="images/portfolio8.jpg">
+          </div>
+        <div class="plus-see base-bg bold">
+          +</div>
+        <div class="pot-title base-bg">
+          This is my first
+          </div>
+            <a href="#" class="overlay"></a>
+         
+    </div>
+    <div class="col4 portfolio-cont-item">
+    	<div class="port-body">
+        	<img src="images/portfolio8.jpg">
+          </div>
+        <div class="plus-see base-bg bold">
+          +</div>
+        <div class="pot-title base-bg">
+          This is my first
+          </div>
+            <a href="#" class="overlay"></a>
+         
+    </div>
+    
+    <div class="clr-fix"></div>
+    
+</div>
+</div>
+<div class="actions">
+<a href="#" class="button uppercase">view my all works </a>
+
+</div>
+</div>
+<div class="about">
+<div class="container">
+<div class="col col12">
+<div class="col6">
+<h2>about me</h2>
+<label class="my-role gray">I'm a UX/UI Designer and Front End Developer from Sydney, Australia.</label>
+<p class="top-mrg-10 gray">I enjoy turning complex problems into simple, beautiful and intuitive interface designs. When I'm not coding or pushing pixels, you'll find me in the gym or on the court shooting hoops.</p>
+<!--<span class="bold">I'm a self-taught interaction & user interface designer based in Berlin,</span> firmly believing that user experience is about the ideas and process, not the artifacts. That's why I've been taking on various design and product development roles in my career to be able to understand all the ins and outs of the process and help you and your developers deliver the product that your users will love.
+
+For the last ten years I have been doing a range of things — from interaction and user interface design through web development to leading product development teams, to make sure I can deliver anything your team needs to get your idea off the ground.
+
+I work for your users and your customers. Let me show you how.-->
+<ul class="inlile-list top-mrg-10 base-color">
+<li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+<li><a href="#"><i class="fa fa-behance"></i></a></li>
+<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+<li><a href="#"><i class="fa fa-github"></i></a></li>
+</ul>
+
+<div class="clr-fix"></div>	
+<hr class="top-mrg-10 white-bg">
+</div>
+<div class="col col12 pdng-lf-none top-mrg-30">
+<div class="col2">
+<div class="my-photo-thumb">
+<a href="#"><img src="images/sliderbg1.jpg"></a>
+</div>
+</div>
+
+<div class="col2">
+<div class="my-photo-thumb">
+<a href="#"><img src="images/sliderbg1.jpg"></a>
+</div>
+</div>
+
+<div class="col2">
+<div class="my-photo-thumb">
+<a href="#"><img src="images/sliderbg1.jpg"></a>
+</div>
+</div>
+
+<div class="col2">
+<div class="my-photo-thumb">
+<a href="#"><img src="images/sliderbg1.jpg"></a>
+</div>
+</div>
+
+</div>
+
+</div>
 </div>
 </div>
 </div>
